a6aec9d50c3884935265f3cebff28d86f02c49d7
Quentin Rasmont <qrasmont@gmail.com>
2022-04-02 11:10:11 +0200

    Create intial ui with bubbletea

--- gc.go
+++ gc.go
@@ -1,7 +1,97 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct {
+	branches []string
+	cursor   int
+	selected map[int]struct{}
+}
+
+func initialModel() model {
+	return model{
+		branches: []string{"test", "test1", "test2"},
+
+		selected: make(map[int]struct{}),
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	case tea.KeyMsg:
+		switch msg.String() {
+
+		case "ctrl+c", "q":
+			return m, tea.Quit
+
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+
+		case "down", "j":
+			if m.cursor < len(m.branches)-1 {
+				m.cursor++
+			}
+
+		case " ":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+		case "enter":
+            // Here we'll execute the delete then quit
+			return m, tea.Quit
+		}
+	}
+
+	return m, nil
+}
+
+func (m model) View() string {
+    // Header
+	s := "Branches \n\n"
+
+	for i, choice := range m.branches {
+
+        // Set the cursor
+		cursor := " "
+		if m.cursor == i {
+			cursor = ">"
+		}
+
+		// Set selection
+		checked := " "
+		if _, ok := m.selected[i]; ok {
+			checked = "x"
+		}
+
+		// Render row
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	}
+
+	// Footer
+    s += "\nQuit: q or ctrl-c\tSelect: space\tDelete: enter\n"
+
+	return s
+}
 
 func main() {
-	fmt.Println("gc")
+	p := tea.NewProgram(initialModel())
+	if err := p.Start(); err != nil {
+		fmt.Printf("error: %v", err)
+		os.Exit(1)
+	}
 }
